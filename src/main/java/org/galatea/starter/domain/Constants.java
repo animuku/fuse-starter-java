52ad784e3813f0d19a53950232c53850aa532bc3
Anirudh <animuku123@gmail.com>
2021-07-07 13:33:50 -0400

    Added dependencies for Redis, fixed codestyle issues.

--- src/main/java/org/galatea/starter/domain/Constants.java
+++ src/main/java/org/galatea/starter/domain/Constants.java
@@ -1,7 +1,8 @@
 package org.galatea.starter.domain;
 
 public final class Constants {
+
   public static final String token = System.getenv("IEX_TOKEN");
-  public static final String rangeURL = "/stock/{symbols}/chart/{range}?token="+token;
-  public static final String dateeURL = "/stock/{symbols}/chart/date/{date}?token="+token;
+  public static final String rangeURL = "/stock/{symbols}/chart/{range}?token=" + token;
+  public static final String dateeURL = "/stock/{symbols}/chart/date/{date}?token=" + token;
 }
--- src/main/java/org/galatea/starter/service/HistoricalPriceClient.java
+++ src/main/java/org/galatea/starter/service/HistoricalPriceClient.java
@@ -20,6 +20,12 @@ public interface HistoricalPriceClient {
   List<IexHistoricalPrice> getHistoricalPricesWithRange(@PathVariable("symbols") String symbols,
       @PathVariable("range") String range);
 
+  /**
+   * Get historical price for each stock symbol passed in.
+   *
+   * @param symbols stock symbols to get historical price for.
+   * @return a list of historical price for each symbol.
+   */
   @GetMapping("/stock/{symbols}/chart/date/{date}?token=${iexToken}")
   List<IexHistoricalPrice> getHistoricalPricesWithDate(@PathVariable("symbols") String symbols,
       @PathVariable("date") String date);
